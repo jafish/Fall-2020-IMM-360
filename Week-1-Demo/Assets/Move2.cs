cae6159da3f3bdc3a77fa91752fdc3cec13b293c
Josh Fishburn <josh@joshfishburn.com>
2020-08-28 23:50:30 -0400

    Complete Week 1 Demo

---
+++ Week-1-Demo/Assets/Move2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(10, 10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
