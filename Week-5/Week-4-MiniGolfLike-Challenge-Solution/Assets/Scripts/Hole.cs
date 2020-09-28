e61fb864b8d4aca967a3b74d1f8475ba5295d085
Josh Fishburn <josh@joshfishburn.com>
2020-09-27 21:58:59 -0400

    Moved Week-4 Challenge Solution to Week 5 Folder

---
+++ Week-5/Week-4-MiniGolfLike-Challenge-Solution/Assets/Scripts/Hole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public GameObject player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.transform.position = new Vector3(0, .25f, 0);
+
+            player.GetComponent<PlayerController>().Reset();
+        }
+    }
+}
---
+++ Week-5/Week-4-MiniGolfLike-Challenge-Solution/Assets/Scripts/Pinwheel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinwheel : MonoBehaviour
+{
+    public float speed;
+    public GameObject paddle1;
+    public GameObject paddle2;
+
+    Vector3 pivot;
+    void Start()
+    {
+        pivot = (paddle1.transform.position + paddle2.transform.position) / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        paddle1.transform.RotateAround(pivot, Vector3.up, speed);
+        paddle2.transform.RotateAround(pivot, Vector3.up, speed);
+    }
+}
