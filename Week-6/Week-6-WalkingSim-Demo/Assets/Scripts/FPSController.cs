ef97ded31555f50129d8258774aa1b74607fb2fa
Josh Fishburn <josh@joshfishburn.com>
2020-10-08 19:53:32 -0400

    Updating Project Locations for Week 7 Release

--- Week-6/Week-6-WalkingSim-Demo/Assets/Scripts/FPSController.cs
+++ Week-6/Week-6-WalkingSim-Demo/Assets/Scripts/FPSController.cs
@@ -10,7 +10,6 @@ public class FPSController : MonoBehaviour
     public CharacterController playerCharacterController;
     public float sensitivity;
     public float speed;
-    public float sprintMultiplier;
     float pitch = 0;
     float yaw = 0;
 
@@ -41,14 +40,8 @@ public class FPSController : MonoBehaviour
 
     void MovePlayer()
     {
-        float sprint = 1;
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
-            sprint = sprintMultiplier;
-        }
-
-        float h = Input.GetAxis("Horizontal") * Time.deltaTime * speed * sprint;
-        float v = Input.GetAxis("Vertical") * Time.deltaTime * speed * sprint;
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
         // Adding horizontal, vertical axes and "gravity"
         Vector3 vel = playerTransform.forward * v + playerTransform.right * h + Vector3.down;
---
+++ Week-7/Week-6-WalkingSim-Challenge-Solution/Assets/Scripts/DayNight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime * speed);
+    }
+}
