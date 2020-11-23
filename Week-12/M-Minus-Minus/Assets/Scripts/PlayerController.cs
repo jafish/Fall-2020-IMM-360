dd86196d2c964834a243a036f85c4ee82509970b
Josh Fishburn <josh@joshfishburn.com>
2020-11-22 23:47:10 -0500

    Complete Demo from Week 12

---
+++ Week-12/M-Minus-Minus/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 forceToApply;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the force we want to apply based on keypress
+        forceToApply = Input.GetAxisRaw("Horizontal") * Vector2.right * 50;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(forceToApply, ForceMode2D.Force);
+    }
+}
---
+++ Week-12/M-Minus-Minus/Assets/Scripts/PlayerControllerDemo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class PlayerControllerDemo : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 forceToApply;
+    public float forceMultiplier;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        forceToApply = Input.GetAxisRaw("Horizontal") * Vector2.right * forceMultiplier;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(forceToApply, ForceMode2D.Force);
+    }
+    
+}
